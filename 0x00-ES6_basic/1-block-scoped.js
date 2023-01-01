a5ae2a36e32d658bf2b709add397914fdb5d20e5
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-01-01 23:05:24 +0100

    done

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -6,7 +6,7 @@ export default function taskBlock(trueOrFalse) {
     var task = true;
     var task2 = false;
   }
-  task = false;
-  task2 = true;
+  // task = false;
+  // task2 = true;
   return [task, task2];
 }
