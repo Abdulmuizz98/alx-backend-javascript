c712b20caa34af12306b1457fbbbf0ce8a4953c7
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-01-03 23:21:41 +0100

    done

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  let result = studentList.filter((e) => e.location === city);
+
+  // console.log(newGrades)
+  result = result.map((e) => {
+    const studentGrade = newGrades.find((i) => i.studentId === e.id);
+    e.grade = studentGrade ? studentGrade.grade : 'N/A';
+    return e;
+  });
+  return result;
+}
