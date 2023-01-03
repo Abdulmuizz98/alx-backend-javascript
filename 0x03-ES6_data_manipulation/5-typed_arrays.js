1b568bcac85523e2b027708288cc150ed2cf8f69
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-01-03 23:41:15 +0100

    task 5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+
+  const int8d = new Int8Array(buffer);
+
+  try {
+    int8d[position] = value;
+  } catch (e) {
+    throw new RangeError();
+  }
+
+  return buffer;
+}
