e570e19faafe59925003044a053ea0a65ea41cb7
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-01-04 12:36:03 +0100

    fix task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanset(set, startString) {
   if (!startString) return [].join('-');
 
-  let result = [...set].filter((x) => x.startsWith(startString));
+  let result = [...set].filter((x) => x && x.startsWith(startString));
   result = result.map((elem) => elem.substring(startString.length));
 
   return result.join('-');
