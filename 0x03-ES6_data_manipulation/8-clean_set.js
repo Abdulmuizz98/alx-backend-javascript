c0332c24a5ebbcfe5095e2e8e30fa34c2c1caec0
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-01-04 01:45:05 +0100

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,12 @@ export default function cleanset(set, startString) {
   let text = '';
   if (!startString) return text;
 
-  const result = [...set].filter((x) => x.startsWith(startString));
-  for (const elem of result) { text += `${elem.substring(startString.length)}-`; }
+  //   const result = [...set].filter((x) => x.startsWith(startString));
+  //   for (const elem of result) { text += `${elem.substring(startString.length)}-`; }
+  //   return text.substring(0, text.length - 1);
+  for (const elem of set) {
+    // console.log(elem, elem.startsWith(startString))
+    if (elem.startsWith(startString)) text += `${elem.substring(startString.length)}-`;
+  }
   return text.substring(0, text.length - 1);
 }
