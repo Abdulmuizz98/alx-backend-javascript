15b2922c0794ae33acf93c59567e884e52c99697
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-02-21 03:24:03 +0100

    done

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,13 @@
 const displayMessage = require('./0-console');
-const { spawn } = require('node:child_process');
+// const { spawn } = require('node:child_process');
 
-
-// readline.question("Welcome to Holberton School, what is your name?\n", name => {
-    //     readline.close()
-    //     // if(!Boolean(process.stdin.isTTY))
-    //     displayMessage("This important software is now closing");
-    // });
 displayMessage("Welcome to Holberton School, what is your name?");
 process.stdin.on('readable', function(){
     const name = process.stdin.read()
     if (name !== null){
         displayMessage("Your name is: " + name);
+    }
+    if(!process.stdin.isTTY){
+        displayMessage("This important software is now closing")
     }
 })
