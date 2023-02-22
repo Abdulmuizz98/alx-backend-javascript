6dc5ece5cbb0f6a105978604f7c8d4efa612888c
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-02-22 07:57:03 +0100

    task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.toString().trimEnd().split('\r\n');
+
+        const csRecords = lines.filter((e) => e.slice(-2) === 'CS');
+        const sweRecords = lines.filter((e) => e.slice(-3) === 'SWE');
+
+        const csFirstnames = csRecords.map((e) => e.split(',')[0]);
+        const sweFirstnames = sweRecords.map((e) => e.split(',')[0]);
+        
+        console.log(`Number of students: ${lines.length - 1}`);
+        console.log(`Number of students in CS: ${cs.length}. List: ${csFirstnames.join(', ')}`);
+        console.log(`Number of students in SWE: ${swe.length}. List: ${sweFirstnames.join(', ')}`);
+        resolve();
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
