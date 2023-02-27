07c3ce832d7415ed70a22e827e3618677a9a19d1
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-02-27 12:19:45 +0100

    done

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,7 +2,20 @@ const request = require('request');
 const {expect} = require('chai');
 
 describe('Cart page', function(){
-    
+
+        
+    it('should send correct status code', function(done){
+        request('http://localhost:7865/', (err, res, body) =>{
+            expect(res.statusCode).to.be.equal(200);
+            done();
+        })
+    })
+    it('should send a plain text message "Welcome to the payment system"', function(done){
+        request('http://localhost:7865/', (err, res, body) =>{
+            expect(body).to.be.equal('Welcome to the payment system');
+            done();
+        })
+    })
     it('should send correct status code when :id is a number', function(done){
         request('http://localhost:7865/cart/16', (err, res, body) =>{
             expect(res.statusCode).to.be.equal(200);
