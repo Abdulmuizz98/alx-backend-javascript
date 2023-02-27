9a365d8445580bf3cf037ab2ce7f12a4175b4a27
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-02-27 04:22:57 +0100

    done

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,8 +1,6 @@
 function getPaymentTokenFromAPI(success){
     if (success){
-        return new Promise ((resolve, reject) => {
-            resolve({data: 'Successful response from the API' })
-        });
+        return Promise.resolve({data: 'Successful response from the API'});
     }
 }
 
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,14 +2,14 @@
 const { expect } = require("chai");
 const getPaymentTokenFromAPI = require("./6-payment_token")
 
-describe('getPaymentTokenFromAPI', function(done){
-    it('should get the payment token for the API like {data: xxx} ', function(){
+describe('getPaymentTokenFromAPI', function(){
+    it('should get the payment token for the API like {data: xxx} ', function(done){
         let result = getPaymentTokenFromAPI(true);
         result
             .then(res => {
                 expect(res).to.have.property('data')
-                expect(res).to.be.equal(1);
+                expect(res.data).to.be.equal('Successful response from the API');
+                done();
             })
-            .then(done);
     })
 })
