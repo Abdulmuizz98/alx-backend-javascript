259b1452f48b73c26a7a30108deec9b31462dbc2
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-02-27 04:14:54 +0100

    done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -4,11 +4,12 @@ const getPaymentTokenFromAPI = require("./6-payment_token")
 
 describe('getPaymentTokenFromAPI', function(done){
     it('should get the payment token for the API like {data: xxx} ', function(){
-        getPaymentTokenFromAPI(true)
-        .then( res => {
-            expect(res).to.have.property('data')
-            expect(res).to.be.equal(1);
-            done();
-        })
+        let result = getPaymentTokenFromAPI(true);
+        result
+            .then(res => {
+                expect(res).to.have.property('data')
+                expect(res).to.be.equal(1);
+            })
+            .then(done);
     })
 })
