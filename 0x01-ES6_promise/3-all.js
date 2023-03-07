f50e627a2e7bd91288bf1ead0f2e5c345aaef013
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-03-08 00:01:00 +0100

    feat: ES6 Promises - implement task 0 - 6

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import { createUser, uploadPhoto } from "./utils";
+
+export default function handleProfileSignup(){
+    Promise.all([uploadPhoto(), createUser()])
+    .then(([res, names]) => {
+        // console.log(res)
+        console.log(res.body, names.firstName, names.lastName);
+    })
+}
