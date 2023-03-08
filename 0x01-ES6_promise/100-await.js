113d9e08adcc94c4f2ae11315a7e619dc443b745
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-03-08 02:02:18 +0100

    fix: task-100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,16 +1,11 @@
-import { createUser, uploadPhoto } from "./utils";
+import { createUser, uploadPhoto } from './utils';
 
-export default async function asyncUploadUser(){
-    const res = await Promise.all([uploadPhoto(), createUser()])
-    try{
-        const photo = await uploadPhoto();
-        const user = await createUser();
-        return { photo, user }
-        
-    }catch(err){
-        return {
-            photo: null,
-            user: null
-        }
-    }
+export default async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
+  } catch (err) {
+    return { photo: null, user: null };
+  }
 }
