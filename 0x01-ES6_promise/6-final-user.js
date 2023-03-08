9314f8e3891d691efac32df09369f8f685e53b58
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-03-08 01:50:23 +0100

    fix: task-6 and task-2

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => res.map((e) => ({ status: e.status, value: e.value ? e.value : e.reason.toString() })));
+    .then((res) => res.map((e) => {
+      const payload = { status: e.status, value: e.value ? e.value : e.reason.toString() };
+      return payload;
+    }));
 }
